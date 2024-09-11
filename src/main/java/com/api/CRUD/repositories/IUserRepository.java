dc4ace9b1c4e4b3c14b7e08f9ea0c1c140b6b81e
unknown <gregori7813@gmail.com>
2024-09-11 15:00:02 -0400

    v1

---
+++ src/main/java/com/api/CRUD/repositories/IUserRepository.java
@@ -0,0 +1,10 @@
+package com.api.CRUD.repositories;
+
+import com.api.CRUD.models.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IUserRepository extends JpaRepository<UserModel,Long> {
+    
+}
